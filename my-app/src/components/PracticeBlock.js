65c0e8bb2f608f500a4e6079039fb9c0408dfe31
Denys Vietrov <denysvietrov@XPUADNIW0009.local>
2020-05-17 17:59:45 +0300

    old kursach

--- my-app/src/components/PracticeBlock.js
+++ my-app/src/components/PracticeBlock.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import './Components.css';
 //assets
-import cylinder from '../assets/images/cylinder.jpg';
+import steelplate from '../assets/images/steelplate.jpg';
 import sheet from '../assets/images/sheet.jpg';
+import cylinder from '../assets/images/cylinder.jpg'
+import many_cylinders from '../assets/images/many-cylinders.jpg'
 //components
 import NavigationBlock from "./NavigationBlock"
 import PracticeProgramm from './practice-components/PracticeProgramm';
+import PracticeProgrammSecond from './practice-components/PracticeProgrammSecond'
+import PracticeProgrammCylinder from './practice-components/PracticeProgrammCylinder'
+import PracticeProgrammCylinderHard from './practice-components/PracticeProgrammCylinderHard'
 
 
 export default class PracticeBlock extends React.Component {
@@ -27,13 +32,14 @@ export default class PracticeBlock extends React.Component {
         };
         return (
             <div className="Main-block">
-                {/* <div
-                    className="Navigation-background"
-                    style={style}
-                /> */}
                 <div className="Practice-inner">
                     <div className="Practice-header">
-                        Practice
+                        <h2>Практична частина</h2>
+                        {this.state.navigationPracticeRoute === "home" &&
+                            <div className="Practice-header-inner">
+                                У цій частині користувач може самостійно випробувати прикладну частину проекту, а саме переглянути рішення задач для запропонованих параметрів або поекспериментувати з власними параметрами. Також він може переглянути графіки залежностей для отриманих рішень
+                            </div>
+                        }
                     </div>
                     {this.state.navigationPracticeRoute === "home" ?
                         <div className="Practice-navigation">
@@ -41,17 +47,42 @@ export default class PracticeBlock extends React.Component {
                                 navigateTo={this.navigatePracticeTo}
                                 image={sheet}
                                 navigateRoute="sheet"
-                                title="ДОСЛІДЖЕННЯ ТЕЧІЇ ДВОШАРОВОЇ РІДКОЇ ПЛІВКИ ПО ПОВЕРХНІ ПЛАСТИНИ" />
+                                title={("Задача про течію двох плівок по поверхні пластини").toUpperCase()} />
+                            <NavigationBlock
+                                navigateTo={this.navigatePracticeTo}
+                                image={steelplate}
+                                navigateRoute="sheet_v2"
+                                title={("Задача про течію двошарової плівки нелінійно-в’язкої рідини по поверхні пластини").toUpperCase()} />
                             <NavigationBlock
                                 navigateTo={this.navigatePracticeTo}
                                 image={cylinder}
                                 navigateRoute="cylinder"
-                                title="ДОСЛІДЖЕННЯ ТЕЧІЇ ДВОШАРОВОЇ РІДКОЇ ПЛІВКИ ПО ПОВЕРХНІ ЦИЛІНДРА" />
+                                title={("ЗАДАЧА ПРО ТЕЧІЮ ДВОШАРОВОЇ РІДКОЇ ПЛІВКИ ПО ПОВЕРХНІ ЦИЛІНДРА").toUpperCase()} />
+                            <NavigationBlock
+                                navigateTo={this.navigatePracticeTo}
+                                image={many_cylinders}
+                                navigateRoute="cylinder_v2"
+                                title={("ЗАДАЧА ПРО ТЕЧІЮ ДВОШАРОВОЇ ПЛІВКИ нелінійно-в’язкої рідини ПО ПОВЕРХНІ ЦИЛІНДРА").toUpperCase()} />
                         </div>
                         :
-                        <div className="Practice-programm">
-                            <PracticeProgramm  navigateTo={this.navigatePracticeTo}/>
-                        </div>}
+                        this.state.navigationPracticeRoute === "sheet" ?
+                            <div className="Practice-programm">
+                                <PracticeProgramm navigateTo={this.navigatePracticeTo} />
+                            </div>
+                            :
+                            this.state.navigationPracticeRoute === "sheet_v2" ?
+                                <div className="Practice-programm">
+                                    <PracticeProgrammSecond navigateTo={this.navigatePracticeTo} />
+                                </div>
+                                : this.state.navigationPracticeRoute === "cylinder" ?
+                                    <div className="Practice-programm">
+                                        <PracticeProgrammCylinder navigateTo={this.navigatePracticeTo} />
+                                    </div>
+                                    :
+                                    <div className="Practice-programm">
+                                        <PracticeProgrammCylinderHard navigateTo={this.navigatePracticeTo} />
+                                    </div>
+                    }
                 </div>
             </div>
         );
---
+++ my-app/src/services/two-phase-skin.js
@@ -0,0 +1,106 @@
+export function calculateTwoPhaseSkin(g, δ1, δ2, p1, p2, υ1, υ2, μ1, μ2, Ge) {
+    const yLength = δ1 + δ2
+    υ1 = υ1 / Math.pow(10, 6)
+    υ2 = υ2 / Math.pow(10, 6)
+    μ1 = μ1 / Math.pow(10, 6)
+    μ2 = μ2 / Math.pow(10, 6)
+    //g – прискорення вільного падіння
+    //p – густина рідини; 
+    //υ i μ - кінематичний та динамічний коефіцієнти в’язкості;
+    //Ge поток газа
+    //δ ширина пленки
+    //y-напруга тертя
+    let speedArray = []
+    let W1, W2;
+    for (let y = 0; y < Number(yLength) + 1; y = y + 0.1) {
+        W1 = (g / υ1) * (-(Math.pow(y, 2) / 2) + (δ1 + ((p2 * δ2 / p1) * (1 - Ge))) * y).toFixed(2)
+        W2 = ((g / υ2) * (-(Math.pow(y, 2) / 2) + (δ1 + δ2 * (1 - Ge) * y + (δ1 * δ1 / 2) * ((υ2 / υ1) - 1) + δ1 * δ2 * ((μ2 / μ1) - 1) * (1 - Ge)))).toFixed(2)
+        speedArray.push({ y, W1: parseFloat(0.995*W1) / Math.pow(10, 6), W2: parseFloat(0.998*W2) / Math.pow(10, 6) })
+    }
+    //оптімальна дія на робочу плівку
+    let q = (δ1 * μ2) / (δ2 * μ1)
+    let r = (1 / 2) * (υ1 / υ2) * (δ1 / δ2) * (δ1 / δ2)
+    let GeOpt = Math.sqrt((2 + 6 * (q + r) + Math.sqrt(1 + 12 * q * q + 6 * (1 + 6 * r) * (q + r))) / (3 + 6 * q))
+    //максимальне та середнє значення швидкості
+    let υ = υ2 / υ1
+    let δ = δ2 / δ1
+    let p = p2 / p1
+    let Wsr = (1 / 3) - (Ge / 2) + ((υ * Math.pow(δ, 2)) / 2) + (δ * υ * p * (1 - Ge));
+    let Wmax = ((1 / 2) * Math.pow((1 - Ge), 2)) + ((υ * Math.pow(δ, 2)) / 2) + (δ * υ * p * (1 - Ge));
+    let calculatedInfo = {
+        speedArray, GeOpt, Wsr, Wmax
+    }
+    return calculatedInfo
+}
+export function calculateTwoPhaseSkinHard(δ1, δ2, p1, p2, Re1, Re2, Fr, n2, n1, Ge) {
+    const yLength = δ1 + δ2
+    let W1, W2, W_1, W_2, W_3, W_4, W_5, ReFr1, ReFr2;
+    ReFr1 = Math.pow((Re1 / Fr), (1 / n1))
+    ReFr2 = Math.pow((Re2 / Fr), (1 / n2))
+    let speedArray = []
+    for (let y = 0; y < Number(yLength) + 1; y = y + 0.1) {
+        W_1 = Math.pow((1 + (p2 * δ2 / δ1 * p1) * (1 + Ge)), ((n1 + 1) / n1))
+        W_2 = Math.pow((1 + (p2 * δ2 / δ1 * p1) * (1 + Ge) - y), ((n1 + 1) / n1))
+        W_3 = Math.pow(((p2 * δ2 / δ1 * p1) * (1 + Ge)), ((n1 + 1) / n1))
+        W_4 = Math.pow(((δ2 / δ1) * (1 + Ge)), ((n2 + 1) / n2))
+        W_5 = Math.pow((1 + ((δ2 / δ1) * (1 + Ge) - y)), ((n2 + 1) / n2))
+        W1 = ReFr1 * (n1 / (n1 + 1)) * (W_1 - W_2)
+        W2 = ReFr1 * (n1 / (n1 + 1)) * (W_1 - W_3) + ReFr2 * (n2 / (n2 + 1)) * (W_4 - W_5)
+        speedArray.push({ y, W1: parseFloat(W1) / Math.pow(10, 6), W2: parseFloat(W2) / Math.pow(10, 6) })
+    }
+    let calculatedInfo = {
+        speedArray
+    }
+    return calculatedInfo
+}
+export function calculateSimpleCylinder(inner_skin, rLength, δ1, δ2, p1, p2, Re1, Re2, Fr, Ge) {
+    //y - радіус, r0 - стартова точка відліку
+    let r0 = 0.9999, speedArray = [], R_spec, R1, R2, R, W1, W2, W_1, lnR, lnR1, ReFr1, ReFr2, R_spec2lnRR1
+    for (let y = 0; y < Number(rLength) + 1; y = y + 0.1) {
+        R = y / r0
+        R1 = Math.abs(inner_skin ? 1 - (δ1 / r0) : 1 + (δ1 / r0))
+        R2 = Math.abs(inner_skin ? 1 - ((δ1 + δ2) / r0) : 1 + ((δ1 + δ2) / r0))
+        ReFr1 = Re1 / (2 * Fr)
+        ReFr2 = Re2 / (2 * Fr)
+        R_spec = Math.sqrt(Math.pow(R2, 2) + 2 * Ge * R2 * (δ2 / r0))
+        W_1 = (p2 / p1) * (-Math.pow(R1, 2) + Math.pow(R_spec, 2))
+        lnR = Math.log(R)
+        lnR1 = Math.log(R1)
+        R_spec2lnRR1 = Math.pow(R_spec, 2) * Math.log(R / R1)
+        W1 = ReFr1 * (((1 - Math.pow(R, 2)) / 2) + (Math.pow(R1, 2) + W_1) * lnR)
+        W2 = ReFr2 * (((Math.pow(R1, 2) + Math.pow(R, 2)) / 2) + R_spec2lnRR1) + ReFr1 * (((1 - Math.pow(R, 2)) / 2) + (Math.pow(R1, 2) + W_1) * lnR1)
+
+        speedArray.push({ y, W1: parseFloat(W1), W2: parseFloat(W2) })
+    }
+    let calculatedInfo = {
+        speedArray
+    }
+    return calculatedInfo
+}
+
+export function calculateHardCylinderSECOND(inner_skin, rLength, δ1, δ2, p1, p2, Re1, Re2, Fr, Ge) {
+    //n=1/3
+    //y - радіус, r0 - стартова точка відліку
+    let r0 = 0.9999, speedArray = [], W1, W2, c_1, c_2, c_3, c_4, R, result1, result2;
+    const a = (Re2 / Math.pow(Fr, 2))
+    for (let y = 0; y < Number(rLength) + 1; y = y + 0.1) {
+        R = y / r0
+        c_2 = Ge * (δ2 / r0) * (Re2 / Math.pow(Fr, 2));
+        c_1 = (p1 / p2) * (Re1 / Re2) * c_2;
+        c_3 = 1 - 8 * a - (12 * c_1 - 24 * Math.pow(a, 2)) - (96 * c_1 * a - 32 * Math.pow(a, 3)) - 96 * Math.pow(c_1, 2) * a + 16 * Math.pow(c_1, 3);
+        c_4 = Math.log(R) * (96 * c_1 * Math.pow(a, 2) - 48 * Math.pow(c_1, 2) - 96 * c_2 * Math.pow(a, 2) + 48 * Math.pow(c_2, 2)) + 12 * Math.pow(R, 2) * (c_1 - c_2) + 96 * R * a * (c_1 - c_2) + ((96 * a) / R) * (Math.pow(c_1, 2) - Math.pow(c_2, 2)) - (16 / Math.pow(R, 2)) * (Math.pow(c_1, 3) - Math.pow(c_2, 3))
+        W1 = ((96 * c_1 * Math.pow(a, 2) - 48 * Math.pow(c_1, 2)) * Math.log(R) - Math.pow(R, 4) + 8 * a * Math.pow(R, 3) + (12 * c_1 - 24 * Math.pow(a, 3)) * Math.pow(R, 2) + (96 * c_1 * a - 32 * Math.pow(a, 3)) * R + ((96 * Math.pow(c_1, 2) * a) / R) + 16 * Math.pow(c_1, 3) / Math.pow(R, 2)) / 32 + c_3;
+        W2 = ((96 * c_2 * Math.pow(a, 2) - 48 * Math.pow(c_2, 2)) * Math.log(R) - Math.pow(R, 4) + 8 * a * Math.pow(R, 3) + (12 * c_2 - 24 * Math.pow(a, 3)) * Math.pow(R, 2) + (96 * c_1 * a - 32 * Math.pow(a, 3)) * R + ((96 * Math.pow(c_2, 2) * a) / R) + 16 * Math.pow(c_2, 3) / Math.pow(R, 2)) / 32 + c_4;
+        if (W1 < 0 || W1 === NaN) {
+            result1 = 0;
+        }
+        else {
+            result1 = W1 * y;
+        }
+        speedArray.push({ y, W1: parseFloat(result1), W2: parseFloat(Math.abs(W2)) })
+    }
+    let calculatedInfo = {
+        speedArray
+    }
+    return calculatedInfo
+}
